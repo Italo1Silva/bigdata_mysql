9cce2e4c90f07ebbc8e57da74a954ac724276247
Italo1Silva <93648869+Italo1Silva@users.noreply.github.com>
2022-03-21 13:46:51 -0500

    Third Fix Connects

--- build.sh
+++ build.sh
@@ -9,8 +9,27 @@ apt-get -y install nodejs
 echo "install datamaker"
 npm install -g datamaker
 
-echo "install mysql-server" 
-apt-get -y install mysql-server
+#descargar MySQL APT Repo https://dev.mysql.com/downloads/file/?id=509020/
+#pasarlo a la MV
+echo "instalar el repo"
+sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
+
+echo "actualizar el repo"
+sudo apt-get update
+
+echo "instalar mysql-shell"
+sudo apt-get install mysql-shell
+#descargar MySQL APT Repo https://dev.mysql.com/downloads/file/?id=509020/
+#pasarlo a la MV
+echo "instalar el repo"
+sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
+
+echo "actualizar el repo"
+sudo apt-get update
+
+echo "instalar mysql-shell"
+sudo apt-get install mysql-shell
+#--------------------------------------------------------------------------
 
 echo "install stunnel to allow an https connection"
 apt-get install stunnel
@@ -22,24 +41,13 @@ echo "Starting stunnel..."
 stunnel
 
 echo "Create the data template"
-echo "SET {{uuid}} '{\"a\":{{integer}},\"txt\":\"{{words 10}}\",\"email\":\"{{email}}\"}'" > template.txt
+echo "INSERT INTO products VALUES ('{{autoinc}}','{{words 5}}', {{price}}, '{{address}}', {{boolean 0.5}});" > template.txt
 
 echo "Create the data. This will take a while because it is creating 350 million rows!"
 cat template.txt | datamaker -i 350000000 > batch.txt
 
 echo "Create the auth token to access mysql"
 echo auth admin <mysql_password_from_terraform_tf_vars> > auth.txt
-
-#descargar MySQL APT Repo https://dev.mysql.com/downloads/file/?id=509020/
-#pasarlo a la MV
-echo "instalar el repo"
-sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
-
-echo "actualizar el repo"
-sudo apt-get update
-
-echo "instalar mysql-shell"
-sudo apt-get install mysql-shell
 
 #ingresar a mysql-shell "mysqlsh"
 echo "connect into mysql"
