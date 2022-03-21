6c21771c992f64e6d48fe40aa3c63ec2083401fd
Italo1Silva <93648869+Italo1Silva@users.noreply.github.com>
2022-03-21 16:58:23 -0500

    Fix Script

--- build.sh
+++ build.sh
@@ -17,36 +17,31 @@ sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
 echo "actualizar el repo"
 sudo apt-get update
 
-echo "instalar mysql-shell"
-sudo apt-get install mysql-shell
-
 echo "instalar mysql-server"
 sudo apt install mysql-server
 #--------------------------------------------------------------------------
-
-echo "install stunnel to allow an https connection"
-apt-get install stunnel
-
-echo "move the stunnel config to the right place"
-mv stunnel.conf /etc/stunnel/
-
-echo "Starting stunnel..."
-stunnel
 
 echo "Create the data template"
 echo "SET {{uuid}} '{\"a\":{{integer}},\"txt\":\"{{words 10}}\",\"email\":\"{{email}}\"}'" > template.txt
 
 echo "Create the data. This will take a while because it is creating 350 million rows!"
-cat template.txt | datamaker -i 350000000 > batch.txt
-
-echo "Create the auth token to access mysql"
-echo auth admin <mysql_password_from_terraform_tf_vars> > auth.txt
+cat template.txt | datamaker -i 3500 > batch.txt
 
 #ingresar a mysql-shell "mysqlsh"
+#Cambiar las variables "h", "P"
 echo "connect into mysql"
-mysql -u Italo_Silva -p 1234567890 -h https://api.eu-gb.databases.cloud.ibm.com/v5/ibm -P 6830
+mysql -u admin -p -h 64d65c2c-9db2-4a8b-b107-897cb2425b83.c1vt02ul0q3fa0509bog.databases.appdomain.cloud -P 31708
 
-echo "Pipe the data into mysql"
+#Crear la BD
+CREATE DATABASE Test;
+
+#Usar la BD
+USE Test;
+
+#Crear la tabla
+REATE TABLE Test (ID int,Last varchar(255)); #Según las columnas necesarias
+
+#ingestar los datos
 LOAD DATA INFILE '/root/batch.txt' 
 INTO TABLE Test
 FIELDS TERMINATED BY ','
