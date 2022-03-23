fe2df4023b6f44f4d4a8aaa88737f8391ec1b0c2
Italo1Silva <93648869+Italo1Silva@users.noreply.github.com>
2022-03-22 21:33:57 -0500

    Casi la c

--- build.sh
+++ build.sh
@@ -12,6 +12,16 @@ npm install -g datamaker
 echo "creando data"
 echo "{{uuid}},{{integer}},{{firstname}} {{surname}},{{words 10}},{{email}}" | datamaker --format csv --iterations 500
 
+#descargar el archivo .deb "https://dev.mysql.com/downloads/repo/apt/"
+echo "instalando repositorio"
+dpkg -i mysql-apt-config_0.8.22-1_all.deb
+
+echo "actualizar repositorio"
+apt-get update
+
+echo "instalar mysql-server"
+apt-get install mysql-server
+
 echo "instalando ibmcloud cli"
 curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
 
